c3d19d456232a8278559a6e4f7b38ef64f07e238
SemonCat <gn00742754@gmail.com>
2014-03-30 02:23:44 +0800

    add Translucent. add Theme.Pink.

---
+++ 2048/src/main/java/com/semoncat/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.semoncat;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+/**
+ * Created by SemonCat on 2014/3/30.
+ */
+public class BaseActivity extends ActionBarActivity{
+
+}
--- 2048/src/main/java/com/semoncat/MainActivity.java
+++ 2048/src/main/java/com/semoncat/MainActivity.java
@@ -1,15 +1,54 @@
 package com.semoncat;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        setupTranslucent();
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.container,new MainFragment()).commit();
+
+        setupSystemBarTintManager();
+    }
+
+    private void setupTranslucent(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // Translucent navigation bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    private void setupSystemBarTintManager(){
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setNavigationBarTintEnabled(true);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintResource(R.drawable.actionBarBackground);
+        tintManager.setNavigationBarTintResource(R.drawable.actionBarBackground);
+
+
     }
 
 }
