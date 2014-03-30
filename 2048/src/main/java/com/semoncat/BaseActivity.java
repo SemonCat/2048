ee84913692530be8b8ef864cee0f62d23857965d
SemonCat <gn00742754@gmail.com>
2014-03-30 20:48:29 +0800

    add Navi drawer.

--- 2048/src/main/java/com/semoncat/BaseActivity.java
+++ 2048/src/main/java/com/semoncat/BaseActivity.java
@@ -1,13 +1,49 @@
 package com.semoncat;
 
+import android.content.res.TypedArray;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.TypedValue;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 /**
  * Created by SemonCat on 2014/3/30.
  */
-public class BaseActivity extends ActionBarActivity{
+public class BaseActivity extends ActionBarActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Pink);
+        super.onCreate(savedInstanceState);
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    public int getActionBarHeight() {
+        int actionBarHeight = 0;
+        TypedValue tv = new TypedValue();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            if (getTheme()!=null &&
+                    getTheme().resolveAttribute(android.R.attr.actionBarSize, tv,
+                    true))
+                actionBarHeight = TypedValue.complexToDimensionPixelSize(
+                        tv.data, getResources().getDisplayMetrics());
+        } else {
+            if (getTheme()!=null &&
+                    getTheme().resolveAttribute(R.attr.actionBarSize, tv,
+                    true))
+                actionBarHeight = TypedValue.complexToDimensionPixelSize(
+                        tv.data, getResources().getDisplayMetrics());
+        }
+        return actionBarHeight;
+    }
 }
--- 2048/src/main/java/com/semoncat/MainActivity.java
+++ 2048/src/main/java/com/semoncat/MainActivity.java
@@ -1,17 +1,26 @@
 package com.semoncat;
 
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends BaseActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        setupDrawer();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container,new MainFragment()).commit();
 
@@ -55,8 +66,52 @@ public class MainActivity extends ActionBarActivity {
             tintManager.setNavigationBarTintResource(a.resourceId);
         }
 
+    }
 
+    private void setupDrawer(){
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.Navi_Drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,    // 讓 Drawer Toggle 知道母體介面是誰
+                R.drawable.ic_navigation_drawer, // Drawer 的 Icon
+                R.string.app_name, // Drawer 被打開時的描述
+                R.string.app_name // Drawer 被關閉時的描述
+        );
 
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        //顯示 Up Button (位在 Logo 左手邊的按鈕圖示)
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //打開 Up Button 的點擊功能
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        View DrawContent = findViewById(R.id.drawer_content);
+        DrawContent.setPadding(0,getActionBarHeight()+getStatusBarHeight(),0,0);
+
+    }
+
+    public void switchTheme(View mView){
+        mDrawerLayout.closeDrawers();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
